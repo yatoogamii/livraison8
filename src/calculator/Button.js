648ff051b0f8fabeb9cd46c7cc0a60fdb2a71deb
yatoogamii <bracaval.elias.pro@gmail.com>
2019-05-02 14:38:30 +0200

    add function for add number and operator in input, and test function for '='

--- src/calculator/Button.js
+++ src/calculator/Button.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default class Button extends React.Component {
   render() {
     return (
-      <button className={this.props.className}>
+      <button className={this.props.className} onClick={this.props.onClick}>
       {this.props.value}
       </button>
     );
--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -3,30 +3,49 @@ import Input from './Input.js';
 import Button from './Button.js';
 
 export default class Calculator extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.showResult = this.showResult.bind(this);
+    this.state = {inputValue: ''};
+  }
+
+  handleClick(operator) {
+    console.log("test");
+    this.setState({inputValue: this.state.inputValue + operator})
+    console.log(typeof(this.state.inputValue));
+  }
+
+  showResult() {
+    console.log("oui");
+  }
   render() {
+    let inputValue = this.state.inputValue;
+
     return (
       <div className="App-calculator">
-          <Input></Input>
-          <Button className="App-calculator__button" value="AC"></Button>
-          <Button className="App-calculator__button" value="Del"></Button>
-          <Button className="App-calculator__button" value="%"></Button>
-          <Button className="App-calculator__button" value="÷"></Button>
-          <Button className="App-calculator__button" value="7"></Button>
-          <Button className="App-calculator__button" value="8"></Button>
-          <Button className="App-calculator__button" value="9"></Button>
-          <Button className="App-calculator__button" value="x"></Button>
-          <Button className="App-calculator__button" value="4"></Button>
-          <Button className="App-calculator__button" value="5"></Button>
-          <Button className="App-calculator__button" value="6"></Button>
-          <Button className="App-calculator__button" value="-"></Button>
-          <Button className="App-calculator__button" value="1"></Button>
-          <Button className="App-calculator__button" value="2"></Button>
-          <Button className="App-calculator__button" value="3"></Button>
-          <Button className="App-calculator__button" value="+"></Button>
-          <Button className="App-calculator__button col-2" value="0"></Button>
-          <Button className="App-calculator__button" value="."></Button>
-          <Button className="App-calculator__button" value="="></Button>
-        </div>
+        <Input value={inputValue}></Input>
+        <Button className="App-calculator__button" value="AC" onClick={ () => this.handleClick("AC") }></Button>
+        <Button className="App-calculator__button" value="Del" onClick={ () => this.handleClick("Del")} ></Button>
+        <Button className="App-calculator__button" value="%" onClick={ () => this.handleClick("%")} ></Button>
+        <Button className="App-calculator__button" value="÷" onClick={ () => this.handleClick("/")} ></Button>
+        <Button className="App-calculator__button" value="7" onClick={ () => this.handleClick("7")} ></Button>
+        <Button className="App-calculator__button" value="8" onClick={ () => this.handleClick("8")} ></Button>
+        <Button className="App-calculator__button" value="9" onClick={ () => this.handleClick("9")} ></Button>
+        <Button className="App-calculator__button" value="x" onClick={ () => this.handleClick("*")} ></Button>
+        <Button className="App-calculator__button" value="4" onClick={ () => this.handleClick("4")} ></Button>
+        <Button className="App-calculator__button" value="5" onClick={ () => this.handleClick("5")} ></Button>
+        <Button className="App-calculator__button" value="6" onClick={ () => this.handleClick("6")} ></Button>
+        <Button className="App-calculator__button" value="-" onClick={ () => this.handleClick("-")} ></Button>
+        <Button className="App-calculator__button" value="1" onClick={ () => this.handleClick("1")} ></Button>
+        <Button className="App-calculator__button" value="2" onClick={ () => this.handleClick("2")} ></Button>
+        <Button className="App-calculator__button" value="3" onClick={ () => this.handleClick("3")} ></Button>
+        <Button className="App-calculator__button" value="+" onClick={ () => this.handleClick("+")} ></Button>
+        <Button className="App-calculator__button col-2" value="0" onClick={ () => this.handleClick("0")} ></Button>
+        <Button className="App-calculator__button" value="." onClick={ () => this.handleClick(".")} ></Button>
+        <Button className="App-calculator__button" value="=" onClick={ () => console.log(eval(inputValue)) } ></Button>
+      </div>
     );
   }
 }
