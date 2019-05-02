1e5e8381c34ab9bbc4705f5fb0c328df419e1488
yatoogamii <bracaval.elias.pro@gmail.com>
2019-05-02 15:47:08 +0200

    Updates

---
+++ precache-manifest.578176c59cf54af2d0a63104ab32a516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae40ce5704150424e82b5e4e230283b4",
+    "url": "/livraison8/index.html"
+  },
+  {
+    "revision": "7922194a9a7cca1f8ab6",
+    "url": "/livraison8/static/css/main.8061868d.chunk.css"
+  },
+  {
+    "revision": "4d78dfcddee8a893a5af",
+    "url": "/livraison8/static/js/2.e72a4c1f.chunk.js"
+  },
+  {
+    "revision": "7922194a9a7cca1f8ab6",
+    "url": "/livraison8/static/js/main.90f514dd.chunk.js"
+  },
+  {
+    "revision": "988a9119c95865e8802c",
+    "url": "/livraison8/static/js/runtime~main.f73985ad.js"
+  }
+]);
